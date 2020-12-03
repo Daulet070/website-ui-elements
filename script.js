629871ee8fd6bfcf5773732eeb38053778038cfe
Daulet <anakhaev07@gmail.com>
2020-12-03 16:53:44 +0300

    UPDATE: expanding-cards commits

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('ok');
+const cardsWrapper = document.querySelector('.cards-wrapper');
+
+cardsWrapper.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.matches('.panel')) {
+        [...cardsWrapper.children].forEach(panel => {
+            panel.classList.remove('active');
+        });
+        target.classList.add('active');
+    }
+});
