055d06b9b16cc08c0afdb3427112e4d1170e18b5
Daulet <anakhaev07@gmail.com>
2020-12-04 16:42:33 +0300

    UPDATE: progress-steps

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const progress = document.getElementById('pogress');
-const progress = document.getElementById('pogress');
-const progress = document.getElementById('pogress');
-const progress = document.getElementById('pogress');
+const progressLine = document.getElementById('pogress');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const stepPoints = document.querySelectorAll('.circle');
+
+let currentActive = 1
+
+nextBtn.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > stepPoints.length) {
+        currentActive = stepPoints.length
+    }
+});
